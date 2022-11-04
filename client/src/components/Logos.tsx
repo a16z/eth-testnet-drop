3c4e8cd3d28cc75cab2ee892daba23c8274a307e
sam <sammage@gmail.com>
2022-11-04 15:04:53 -0700

    new logo

--- client/src/components/Logos.tsx
+++ client/src/components/Logos.tsx
@@ -4,13 +4,13 @@ const Logos = () => {
             {/* Vertical stack */}
             {/* <div className="items-center text-md">
                 <img src="/ef-logo.webp" className="h-20 mt-5"></img>
-                <img src="/a16zcrypto-logo.svg" className="h-20 mt-5"></img>
+                <img src="/a16z-crypto-logo.svg" className="h-20 mt-5"></img>
             </div> */}
 
             {/* Horizontal stack */}
-            <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 items-center content-center justify-center flex place-items-center">
                 <img src="/ef-logo.webp" className="h-20 m-5" alt="ef logo"></img>
-                <img src="/a16zcrypto-logo.svg" className="h-20 m-5" alt="a16z crypto logo"></img>
+                <img src="/a16z-crypto-logo.svg" className="h-20 m-5" alt="a16z crypto logo"></img>
             </div>
         </div>
     )
