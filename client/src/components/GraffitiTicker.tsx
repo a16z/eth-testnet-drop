0db3f1c5e953a48358e42909a3b116483dc34d42
sam <sammage@gmail.com>
2022-12-08 13:16:30 -0800

    clean up graffiti logs query; css remove scrollbar chrome

--- client/src/components/GraffitiTicker.tsx
+++ client/src/components/GraffitiTicker.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Contract, ethers } from 'ethers';
 import Marquee from "react-fast-marquee";
-import CurrentConfig from '../config';
-import CollectorAbi from "../ABIs/Collector.json";
 import { profanity } from '@2toad/profanity';
+import { getAllCollectionEvents } from '../utils/query-logs';
 
 
 interface Message {
@@ -18,43 +16,20 @@ const GraffitiTicker = () => {
 
     // Query for deposit logs
     useEffect(() => {
-        CurrentConfig.Chains.map(chainConfig => {
-            let rpcUrl = chainConfig.Chain.rpcUrls.default;
-            let network = chainConfig.Chain.id;
-            let address = chainConfig.ContractAddr;
-            let provider = new ethers.providers.JsonRpcProvider(rpcUrl, network);
-            let contract = new Contract(address, CollectorAbi.abi, provider);
-            let filter = contract.filters.Claim();
-            provider.getBlockNumber()
-                .then(blockNumber => {
-                    let fromBlock = blockNumber - CurrentConfig.GraffitiMaxBlocks;
-                    fromBlock = fromBlock > 0 ? fromBlock : 0;
-                    contract.queryFilter(filter, fromBlock)
-                        .then(async events => {
-                            let newMessages = await Promise.all(events.map(async (evt) => {
-                                let block = await evt.getBlock();
-                                let censored = profanity.censor(evt!.args!['graffiti']);
-                                let msg: Message = {
-                                    address: evt!.args!['sender'],
-                                    message: censored,
-                                    timestamp: block.timestamp,
-                                    tx: evt.transactionHash
-                                }
-                                return msg;
-                            }));
-                            newMessages = newMessages.filter((msg) => msg.message !== "");
-
-                            // Aggregate, sort, dedupe
-                            messages = messages.concat(newMessages);
-                            messages = messages.sort((a, b) => b.timestamp - a.timestamp);
-                            let filteredMessages = messages.filter((msg, index) => messages.findIndex(msg2 => msg2.tx === msg.tx) === index);
-
-                            setMessages(filteredMessages);
-                        })
-                        .catch(err => console.error(err))
-                })
-                .catch(err => console.error(err))
-        })
+        let fetch = async () => {
+            let collectionEvents = await getAllCollectionEvents()
+            let msgs = collectionEvents
+                .filter(evt => evt.message !== "")
+                .map(evt => {
+                    return {
+                        address: evt.address, 
+                        message: profanity.censor(evt.message), 
+                        timestamp: evt.timestamp, 
+                        tx: evt.tx
+                    } as Message})
+            setMessages(msgs);
+        }
+        fetch().catch(console.error)
     }, [])
 
     let speed = window.innerWidth > 500 ? 15 : 6;
