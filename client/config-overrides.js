f8836faa6ae4a2bd44644403c634b326cbc21f3a
sam <sammage@gmail.com>
2022-12-08 13:28:25 -0800

    rework buffer overrides; reinstall yarn.lock

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,9 +1,17 @@
 // CRA react-app-rewired override
+const webpack = require("webpack")
+
 module.exports = function override (config, env) {
     let loaders = config.resolve
     loaders.fallback = {
-        "Buffer": require.resolve("buffer/"),
+        "buffer": require.resolve("buffer"),
         "crypto": require.resolve("crypto-browserify")
     }
+    config.plugins = [
+        ...config.plugins,
+        new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ]
     return config
 }
